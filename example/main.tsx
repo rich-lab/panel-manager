4740f29b8f929c375160eb9e46ef32a3b78c5248
Kbscript <koubodehao@gmail.com>
2022-04-22 05:33:59 +0000

    Update main.tsx and package.json

--- example/main.tsx
+++ example/main.tsx
@@ -1,47 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './example.css';
-import 'normalize.css';
+import "./example.css";
+import "normalize.css";
 
-import PanelManager from '../src';
+import PanelManager from "../src";
 
 const defaultPanelSize = {
-  LTL: { width: '320px', height: 'auto' },
-  R: { width: '500px', height: 'auto' },
-  LB: { width: 'auto', height: '400px' },
+  LTL: { width: "320px", height: "auto" },
+  R: { width: "500px", height: "auto" },
+  LB: { width: "auto", height: "400px" },
 };
 
 const mockPanel = {
   R: {
-    component: <div className="example-panel-content">R</div>,
+    component: <div className="example-panel-content">Right Panel</div>,
     resizable: true,
     resizeConfig: {
       defaultSize: defaultPanelSize.R,
-    }
+    },
   },
   LB: {
-    component: <div className="example-panel-content">LB</div>,
+    component: <div className="example-panel-content">Left Panel</div>,
     resizable: true,
     resizeConfig: {
       defaultSize: defaultPanelSize.LB,
-    }
+    },
   },
   LTL: {
     component: <div className="example-panel-content">Panel</div>,
     resizable: true,
     resizeConfig: {
       defaultSize: defaultPanelSize.LTL,
-    }
+    },
   },
   LTR: <div className="example-panel-content linear-wipe">Manager</div>,
 };
 
 ReactDOM.render(
   <React.StrictMode>
-    <div style={{ height: '100vh', width: '100vw' }}>
+    <div style={{ height: "100vh", width: "100vw" }}>
       <PanelManager panelConfig={mockPanel} disableCache autoBorder />
     </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
